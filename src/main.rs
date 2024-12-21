9b2c86769d936a51f13f32dfcaa6fce280e81e45
Mia <miabernhardt75@gmail.com>
2024-12-21 16:24:50 +0100

    rainbow ball :D

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,19 @@ const WIDTH: i32 = 220;
 const HEIGHT: i32 = 220;
 
 fn main() {
-    let (mut raylib_handle, thread) = raylib::init()
-        .size(WIDTH, HEIGHT)
-        .title("nudelsim")
-        .build();
+    let (mut raylib_handle, thread) = raylib::init().size(WIDTH, HEIGHT).title("nudelsim").build();
     raylib_handle.set_target_fps(60);
 
     while !raylib_handle.window_should_close() {
         let mut draw_handle = raylib_handle.begin_drawing(&thread);
 
         draw_handle.clear_background(Color::BLACK);
-        draw_handle.draw_circle(WIDTH / 2, HEIGHT / 2, 48.0, Color::BLUE);
+        draw_handle.draw_circle(
+            WIDTH / 2,
+            HEIGHT / 2,
+            48.0,
+            Color::color_from_hsv(draw_handle.get_time() as f32 * 120.0, 1.0, 1.0),
+        );
         draw_handle.draw_fps(10, 10);
     }
 }
