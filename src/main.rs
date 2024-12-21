80563f4c40d33c3f526004558b01a858b1b30521
Mia <miabernhardt75@gmail.com>
2024-12-21 15:58:55 +0100

    changed bg color and title

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,17 @@ const WIDTH: i32 = 220;
 const HEIGHT: i32 = 220;
 
 fn main() {
-    let (mut raylib_handle, thread) = raylib::init().size(WIDTH, HEIGHT).title("Hello, World").build();
+    let (mut raylib_handle, thread) = raylib::init()
+        .size(WIDTH, HEIGHT)
+        .title("nudelsim")
+        .build();
+    raylib_handle.set_target_fps(60);
 
     while !raylib_handle.window_should_close() {
         let mut draw_handle = raylib_handle.begin_drawing(&thread);
 
-        draw_handle.clear_background(Color::WHITE);
-        draw_handle.draw_circle(WIDTH/2, HEIGHT/2, 48.0, Color::BLACK);
-        draw_handle.draw_fps(10,10);
+        draw_handle.clear_background(Color::BLACK);
+        draw_handle.draw_circle(WIDTH / 2, HEIGHT / 2, 48.0, Color::BLUE);
+        draw_handle.draw_fps(10, 10);
     }
 }
