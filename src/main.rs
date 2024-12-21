14020172393136de5070eb5c1f3b1594a1ba79a0
Mia <miabernhardt75@gmail.com>
2024-12-21 17:08:42 +0100

    implemented deltatime

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use raylib::prelude::*;
 
 const WIDTH: i32 = 2560;
 const HEIGHT: i32 = 1080;
+const WANTED_FPS: i32 = 60;
 const GRAVITY: Vector2 = Vector2::new(0., 9.8);
 
 #[derive(Debug)]
@@ -28,18 +29,18 @@ impl Ball {
             self.color,
         )
     }
-    fn gravity(&mut self, frame_time: f32) {
+    fn gravity(&mut self, dt: f32) {
         dbg!(self.velocity);
-        self.velocity += GRAVITY * frame_time;
+        self.velocity += GRAVITY * dt;
     }
-    fn apply_speed(&mut self, frame_time: f32) {
-        self.position += self.velocity * frame_time;
+    fn apply_speed(&mut self, dt: f32) {
+        self.position += self.velocity * dt;
     }
 }
 
 fn main() {
     let (mut raylib_handle, thread) = raylib::init().size(WIDTH, HEIGHT).title("nudelsim").build();
-    raylib_handle.set_target_fps(60);
+    raylib_handle.set_target_fps(WANTED_FPS as u32);
     let mut ball = Ball::new(
         Vector2::new((WIDTH / 2) as f32, 0.),
         Vector2::new(0., 0.),
@@ -49,10 +50,13 @@ fn main() {
 
     while !raylib_handle.window_should_close() {
         let frame_time = raylib_handle.get_frame_time();
+        let dt = frame_time / (1.0 / WANTED_FPS as f32);
+        dbg!(dt);
+
         let mut draw_handle = raylib_handle.begin_drawing(&thread);
         ball.draw(&mut draw_handle);
-        ball.gravity(frame_time);
-        ball.apply_speed(frame_time);
+        ball.gravity(dt);
+        ball.apply_speed(dt);
 
         draw_handle.clear_background(Color::WHITE);
     }
